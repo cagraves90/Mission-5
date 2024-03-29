d18463da416e00588db4f62125db33f8fff325dd
Chelsea G <chelseag@missionreadyhq.com>
2024-03-29 15:34:27 +1300

    Added one listing to modal component and style

--- mission-5-frontend/src/components/TradeMeCompareModal.tsx
+++ mission-5-frontend/src/components/TradeMeCompareModal.tsx
@@ -4,6 +4,57 @@ import { Dialog, Transition } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faScaleUnbalanced } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+const FakeListingTwo = [
+  {
+    listingImage: "./images/TradeMe_PaddleChair.png",
+    listingArea: "Auckland",
+    listingCloseTime: "Closes Sun 15 Jun 2024",
+    listingName: "Paddle Chair",
+    listingDetails:
+      "Designed by the talented Danish designer Hans. J. Wegner, This is a handmade classic of comfort and craft. Gracefully sculpted. A one-piece base construction for strength. Woven skillfully back and front of durable paper yarn, the Paddle Chair is remarkably comfortable. An investment that will gain value over time.",
+    buyItNowPrice: "$2950",
+    startingBidPrice: "$1650",
+    itemCondition: [
+      <div>
+        <FontAwesomeIcon icon={faCheck} /> New
+      </div>,
+    ],
+    sellersRating: [
+      <FontAwesomeIcon icon={faStar} />,
+      <FontAwesomeIcon icon={faStar} />,
+      <FontAwesomeIcon icon={faStar} />,
+      <FontAwesomeIcon icon={faStar} />,
+      <FontAwesomeIcon icon={faStar} />,
+    ],
+    itemShipping: [
+      <div>
+        <FontAwesomeIcon icon={faCheck} /> Parcel Collect
+      </div>,
+    ],
+    itemPaymentOption: [
+      <div>
+        <FontAwesomeIcon icon={faCheck} /> NZ Bank Deposit
+      </div>,
+    ],
+  },
+  //   {
+  //     listingImage: "./images/TradeMe_PaddleChair.png",
+  //     listingArea: "Auckland",
+  //     listingCloseTime: "Closes Sun 15 Jun 2024",
+  //     listingName: "Paddle Chair",
+  //     listingDetails:
+  //       "Designed by the talented Danish designer Hans. J. Wegner, This is a handmade classic of comfort and craft. Gracefully sculpted. A one-piece base construction for strength. Woven skillfully back and front of durable paper yarn, the Paddle Chair is remarkably comfortable. An investment that will gain value over time.",
+  //     buyItNowPrice: "$2950",
+  //     startingBidPrice: "$1650",
+  //     itemCondition: "New",
+  //     sellersRating: "100%",
+  //     itemShipping: "Parcel Collect",
+  //     itemPaymentOption: "NZ Bank Deposit",
+  //   },
+];
 
 const TradeMeCompareModal = () => {
   let [isOpen, setIsOpen] = useState(false);
@@ -53,19 +104,56 @@ const TradeMeCompareModal = () => {
                       leaveTo="opacity-0 scale-95"
                     >
                       <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                        {/* --------------------------------- Carousel Portion of the Modal ------------------------------------- */}
                         <Dialog.Title
                           as="h3"
                           className="text-lg font-medium leading-6 text-gray-900"
                         >
-                          Payment successful
+                          {/* This will need a map eventually ------------------------------------------------------ */}
                         </Dialog.Title>
-                        <div className="mt-2">
-                          <p className="text-sm text-gray-500">
-                            Your payment has been successfully submitted. We’ve
-                            sent you an email with all of the details of your
-                            order.
-                          </p>
-                        </div>
+                        {FakeListingTwo.map((item) => (
+                          <div className="mt-2">
+                            <img src={item.listingImage} alt="" />
+                            <div className="text-xl flex flex-col">
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Closing Time
+                              </p>
+                              {item.listingCloseTime}
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                No reserve
+                              </p>
+                              {item.startingBidPrice}
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Buy Now
+                              </p>
+                              {item.buyItNowPrice}
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Location
+                              </p>
+                              {item.listingArea}
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Seller's Rating
+                              </p>
+                              <div className="flex">{item.sellersRating}</div>
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Condition
+                              </p>
+                              <div className="flex">{item.itemCondition}</div>
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Shipping
+                              </p>
+                              {item.itemShipping}
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Payment
+                              </p>
+                              {item.itemPaymentOption}
+                              <p className="flex justify-center bg-[#ffb00b] rounded-md my-1">
+                                Description
+                              </p>
+                              {item.listingDetails}
+                            </div>
+                          </div>
+                        ))}
 
                         <div className="mt-4">
                           <button
